8ec5e75a67f9f25ce05ef8b95faeedb56f062913
Ricardo Santos <ricardossantos.dev@gmail.com>
2024-06-10 09:15:28 -0300

    Passando props através da desestruturação.

--- teoria/aulas/teste02/src/Sport.js
+++ teoria/aulas/teste02/src/Sport.js
@@ -3,7 +3,6 @@ import { getImgUrl } from './galery'
 export function Sport({ sport, size }) {
   return (
     <>
-      
       <img 
         className="avatar"
         src={getImgUrl(sport)}
@@ -14,3 +13,24 @@ export function Sport({ sport, size }) {
     </>
   )
 }
+
+/*
+Podemos atribuir ao componente apenas um atributo de prop e capturar cada prop passado pelo componente pai em uma variável e trabalhar dentro de nosso componente. Veja que, ao invez de passar dois props dentro das chaves, { sport, size }, passamos apenas um valor chamdo props e criamos cada variável respectivamente ao prop que vai ser usado. Esta sintaxe é chamada de “desestruturação” e é equivalente a ler propriedades de um parâmetro de função.
+
+export function Sport(props) {;
+  let sport = props.sport
+  let sportName = props.sport.sportName;
+  let size = props.size;
+  return (
+    <>
+      <img 
+        className="avatar"
+        src={getImgUrl(sport)}
+        alt={sportName}
+        width={size}
+        height={size}
+      />
+    </>
+  )
+}
+*/
